515d0f1d19b4f039b1ea5ddf0042ca800f509650
Dave Sersch <davesersch@Yelenas-MacBook-Pro.local>
2017-08-01 14:24:25 -0700

    added reset, score, rndom cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,12 +23,29 @@ var cards = [
   }
 ];
 
+Array.prototype.shuffle = function() {
+  var input = this;
+  for (var i = input.length - 1; i >= 0; i--) {
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = input[randomIndex];
+
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
+};
+
+cards.shuffle();
 var cardsInPlay = [];
+var totalScore = 0;
 
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You've found a match!");
+      totalScore++;
+      console.log("Score: " + totalScore);
+      document.getElementById('score').textContent = "TOTAL SCORE: " + totalScore;
     } else {
       alert("Sorry, try again.");
     }
@@ -45,14 +62,31 @@ var flipCard = function () {
   checkForMatch();
 };
 
+var firstRound = true;
+
 var createBoard = function() {
-  for (var i = 0; i < cards.length; i++) {
-    var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild(cardElement);
+  if (firstRound === true) {
+    for (var i = 0; i < cards.length; i++) {
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard);
+      document.getElementById('game-board').appendChild(cardElement);
+      firstRound = false;
+    }
+  } else {
+    for (var i = 0; i < cards.length; i++) {
+      document.getElementsByTagName('img')[i].setAttribute('src', 'images/back.png');
+    }
   }
 };
 
+var resetGame = function() {
+  cards.shuffle();
+  cardsInPlay = [];
+  createBoard();
+};
+
 createBoard();
+
+document.getElementById('reset').addEventListener('click', resetGame);
